dca726c3c6aefd06abd8f890cfac878fc19d77d0
Leandro Vicensotto <cmdleandro@gmail.com>
2025-07-17 11:15:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mmg.whatsapp.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
